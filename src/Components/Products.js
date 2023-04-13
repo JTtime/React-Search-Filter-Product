d26678189b5c497662288a10f9ffa7c5498fb929
JTtime <105555233+JTtime@users.noreply.github.com>
2023-04-13 15:11:21 +0000

    products card layout

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,9 +1,48 @@
-import React from "react";
+import React, {useState, useEffect, useContext} from "react";
+import MyContext from "./MyContext";
 
 export default function Products() {
+    const [productsData, setProductsData] = useState([]);
+    const [filteredData, setFilteredData] = useState([]);
+
+    async function fetchData() {
+        const data = await fetch("https://fakestoreapi.com/products").then((res) =>
+          res.json()
+        );
+    
+        setProductsData(data);
+      }
+      useEffect(() => {
+        fetchData();
+      }, []);
+
+      useEffect(() => {
+        setFilteredData(productsData);
+        console.log(filteredData);
+      }, [productsData]);
+
+
     return (
         <>
-        this is Products Listing
+        <div>
+        <div className="productsList">
+          {filteredData.map((ele) => (
+            <div className="productCard" key={ele.id}>
+               <div class="card">
+                    <img
+                      src={ele.image}
+                      alt={ele.title}
+                      className="thumbnail"
+                    />
+                    <div className="card_caption">
+                      <div>{ele.title}</div>
+                      <div>Price: $ {ele.price}</div>
+                    </div>
+                  </div>
+            </div>
+          ))}
+        </div>
+      </div>
         </>
     )
 }
