03a5b92b0b40e9b3bb8e92197459637176fc561f
JTtime <jeevraj.vjti@gmail.com>
2023-04-14 12:49:06 +0530

    wishlist implementation

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect, useContext} from "react";
 import {MyContext} from "./MyContext";
+import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
 
 export default function Products() {
     const {
@@ -8,6 +9,17 @@ export default function Products() {
         filteredData,
         setFilteredData
       } = useContext(MyContext);
+      const [favorite, setFavorite] = useState([]);
+
+      function handleHeartClick(id) {
+        if (favorite.includes(id)) {
+          setFavorite(favorite.filter((i) => i !== id));
+        }
+        else {
+          setFavorite([...favorite, id]);
+        }
+
+      }
 
     async function fetchData() {
         const data = await fetch("https://fakestoreapi.com/products").then((res) =>
@@ -29,19 +41,29 @@ export default function Products() {
     return (
         <>
         <div>
-        <div className="productsList">
+            <div className="productsList">
           {filteredData.map((ele) => (
-            <div className="productCard" key={ele.id}>
-               <div class="card">
-                    <img
-                      src={ele.image}
-                      alt={ele.title}
-                      className="thumbnail"
-                    />
+                <div className="productCard" key={ele.id}>
+                    <div class="card">
+                       <div className="image-container">
+                        <img
+                          src={ele.image}
+                          alt={ele.title}
+                          className="thumbnail"
+                          />
+                          <div className="heart" onClick={()=> handleHeartClick(ele.id)}>                      
+                      {
+                        favorite.includes(ele.id) ? <AiFillHeart className="clickred"/> : <AiOutlineHeart/>
+                        
+                      }
+                          </div>
+                          
+                        </div>
                     <div className="card_caption">
                       <div>{ele.title}</div>
                       <div>Price: $ {ele.price}</div>
                     </div>
+
                   </div>
             </div>
           ))}
